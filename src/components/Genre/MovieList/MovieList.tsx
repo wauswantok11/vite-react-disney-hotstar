f74265f441cf5661526b5bbd1732f294d85eb529
Boom Adul <wauswantok11@gmail.com>
2025-02-10 22:24:39 +0700

    fix

--- src/components/Genre/MovieList/MovieList.tsx
+++ src/components/Genre/MovieList/MovieList.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import service from "../../../service/ApiMoviedb";
 import { DiscoverListDTO } from "../../../service/DiscoverListDTO";
-const IMAGE_URL = "https://image.tmdb.org/t/p/original";
+// const ImageUrl = "https://image.tmdb.org/t/p/original";
 import styles from "./MovieList.module.scss";
 interface MovieListProps {
   genreID: string;
 }
+
 const MovieList: React.FC<MovieListProps> = ({ genreID }) => {
   const [movies, setMovies] = useState<DiscoverListDTO>();
   const getPosterListById = async (id: string) => {
@@ -22,9 +23,9 @@ const MovieList: React.FC<MovieListProps> = ({ genreID }) => {
   return (
     <>
       <div className={styles.MoviePosterList}>
-        {movies?.results.map((item) => (
+        {movies?.results?.map((item) => (
           <div className={styles.MoviePosterItem}>
-            <img src={IMAGE_URL + item.backdrop_path} alt={item.title} />
+            <img src={"https://image.tmdb.org/t/p/original" + item.backdrop_path} alt={item.title} />
           </div>
         ))}
       </div>
